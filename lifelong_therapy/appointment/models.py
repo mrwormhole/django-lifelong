0bfaf107665040d89fea8a89b34816a609a4fe78
mrwormhole <talhaaltinel@hotmail.com>
2020-02-24 17:10:42 +0300

    utility func

--- lifelong_therapy/appointment/models.py
+++ lifelong_therapy/appointment/models.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.db import models
 from django.core.validators import RegexValidator
+from django.utils import timezone
 
 class Therapist(models.Model):
     therapist_name = models.CharField(max_length = 40)
@@ -22,5 +25,6 @@ class Patient(models.Model):
     def __str__(self):
         return self.patient_name
 
-
+    def is_appointment_date_near(self):
+        return self.appointment_date <= timezone.now() + datetime.timedelta(days = 3)
 
